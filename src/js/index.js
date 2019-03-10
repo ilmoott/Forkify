b747cca40d79e6474f8e5d9857628be179283298
Ilmo Ott <ilmo.ott@hotmail.com>
2019-03-10 20:10:28 +1000

    Search part 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,38 @@
-import axios from "axios";
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import {elements} from './views/base';
 
-const key = "455a45cbee88efd00ed2b3ec019c80a3";
-const url = "https://www.food2fork.com/api/search";
+//global state of the app
+/*
+- Search object
+- Current recipe
+- Shopping list
+- Liked recipes
+ */
+const state = {};
 
-async function getResults(query){
+const controlSearch = async ()=>{
+    //1. Get the query from the view
+    const query = searchView.getInput();
 
-    try{
-        const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${query}`);
-        const recipes = res.data.recipes;
-        console.log(recipes);
-    }catch(error){
-        alert(error);
+    if (query){
+        //2. New Search Obj and add it to state
+        state.search = new Search(query);
+
+        //3. Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        //4. Search for recipes
+        await state.search.getResults();
+
+        //5. render results on UI
+        searchView.renderResults(state.search.result);
     }
-    
-};
+}
 
-getResults('pizza');
+elements.searchForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    controlSearch();
+})
+
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,22 @@
-export default 'I am an exported string.';
+import axios from 'axios';
+
+export default class Search {
+    constructor(query){
+        this.query = query;
+    }
+
+    async getResults(){
+
+        const key = "455a45cbee88efd00ed2b3ec019c80a3";
+        const url = "https://www.food2fork.com/api/search";
+
+        try{
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+        }catch(error){
+            alert(error);
+        }
+        
+    };
+}
+
